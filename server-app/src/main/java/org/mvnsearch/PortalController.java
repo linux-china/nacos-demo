597481a505528b1a358919eb9dd60499d6dd5e84
linux_china <libing.chen@gmail.com>
2021-02-04 21:36:55 -0800

    PortalController

---
+++ server-app/src/main/java/org/mvnsearch/PortalController.java
@@ -0,0 +1,14 @@
+package org.mvnsearch;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PortalController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello serverApp!";
+    }
+
+}
