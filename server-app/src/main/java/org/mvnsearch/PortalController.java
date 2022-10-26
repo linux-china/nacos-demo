bce860bef1f1a6a5572c49985d5818498048668a
linux_china <libing.chen@gmail.com>
2022-10-26 17:47:05 +0800

    chore: add admin property

--- server-app/src/main/java/org/mvnsearch/PortalController.java
+++ server-app/src/main/java/org/mvnsearch/PortalController.java
@@ -1,14 +1,20 @@
 package org.mvnsearch;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class PortalController {
+
+    @Value("${admin}")
+    private String admin;
 
     @GetMapping("/")
     public String index() {
-        return "Hello serverApp!";
+        return "Hello " + admin + "!";
     }
 
 }
